65b614b1835504a365270ec6f01b334d7b8b6222
Stephan Aßmus <stephan.assmus@gmx.de>
2023-11-13 12:37:08 +0100

    Refactor to use useEffect and useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -4,62 +4,51 @@ import OpenAIConfig from "./OpenAIConfig.ts";
 
 const openai = new OpenAI(OpenAIConfig);
 
-let mediaRecorder: MediaRecorder;
-let audioChunks: Blob[] = [];
 const mimeType = 'audio/webm';
 
-let sendToWhisperAPI: (audioChunks: Blob[]) => Promise<void>;
-
-const startRecording = () => {
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-    .then(stream => {
-      audioChunks = [];
-      mediaRecorder = new MediaRecorder(stream, { mimeType });
-      
-      mediaRecorder.ondataavailable = (event) => {
-        if (event.data.size > 0) {
-          audioChunks.push(event.data);
-        }
-      };
-      
-      mediaRecorder.onstop = () => {
-        console.log('stopped MediaRecorder');
-        sendToWhisperAPI(audioChunks);
-      };
-      
-      console.log('started MediaRecorder, MIME type:', mediaRecorder.mimeType);
-      mediaRecorder.start(1000);
-    })
-    .catch(error => {
-      console.log('Failed to start recorder', error);
-    });
-};
-
-const stopRecording = () => {
-  if (mediaRecorder) {
-    mediaRecorder.stop();
-  } else {
-    console.log('MediaRecorder undefined');
-  }
-};
-
 const App = () => {
-  const [listening, setListening] = React.useState(false);
-  const [conversationOpen, setConversationOpen] = React.useState(false);
-  const [transcript, setTranscript] = React.useState('');
-  const [message, setMessage] = React.useState('');
+  const [listening, setListening] = useState(false);
+  const [conversationOpen, setConversationOpen] = useState(false);
+  const [transcript, setTranscript] = useState('');
+  const [message, setMessage] = useState('');
   
-  const startConversation = () => {
-    setConversationOpen(true);
-    startRecording();
-  }
+  let mediaRecorder;
+  let audioChunks = [];
   
-  const stopConversation = () => {
-    setConversationOpen(false);
-    stopRecording();
-  }
+  const startRecording = useCallback(() => {
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+      .then(stream => {
+        audioChunks = [];
+        mediaRecorder = new MediaRecorder(stream, { mimeType });
+        
+        mediaRecorder.ondataavailable = (event) => {
+          if (event.data.size > 0) {
+            audioChunks.push(event.data);
+          }
+        };
+        
+        mediaRecorder.onstop = () => {
+          console.log('stopped MediaRecorder');
+          sendToWhisperAPI(audioChunks);
+        };
+        
+        console.log('started MediaRecorder, MIME type:', mediaRecorder.mimeType);
+        mediaRecorder.start(1000);
+      })
+      .catch(error => {
+        console.log('Failed to start recorder', error);
+      });
+  }, []);
   
-  sendToWhisperAPI = async (audioChunks: Blob[]) => {
+  const stopRecording = useCallback(() => {
+    if (mediaRecorder) {
+      mediaRecorder.stop();
+    } else {
+      console.log('MediaRecorder undefined');
+    }
+  }, []);
+  
+  const sendToWhisperAPI = useCallback(async (audioChunks) => {
     const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
     
     try {
@@ -73,9 +62,19 @@ const App = () => {
     } catch (error) {
       console.error('Failed to send request to Whisper API', error);
     }
-  };
+  }, []);
   
-  React.useEffect(() => {
+  const startConversation = useCallback(() => {
+    setConversationOpen(true);
+    startRecording();
+  }, [startRecording]);
+  
+  const stopConversation = useCallback(() => {
+    setConversationOpen(false);
+    stopRecording();
+  }, [stopRecording]);
+  
+  useEffect(() => {
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
     
@@ -89,14 +88,10 @@ const App = () => {
     
     recognition.onresult = (event) => {
       for (let i = event.resultIndex; i < event.results.length; i++) {
-        const transcript = event.results[i][0].transcript.trim();
-        if (event.results[i].isFinal) {
-          if (transcript.toLowerCase().includes('hallo computer')) {
-            startConversation();
-          }
-          setTranscript(transcript);
-        } else {
-          setTranscript(transcript);
+        const currentTranscript = event.results[i][0].transcript.trim();
+        setTranscript(currentTranscript);
+        if (event.results[i].isFinal && currentTranscript.toLowerCase().includes('hallo computer')) {
+          startConversation();
         }
       }
     };
@@ -110,7 +105,7 @@ const App = () => {
     return () => {
       recognition.stop();
     };
-  }, []);
+  }, [startConversation]);
   
   return (
     <div>
