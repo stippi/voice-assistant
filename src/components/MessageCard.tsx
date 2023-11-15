544556d5ed1e68a91d6f4610d3c8dd5f7b7f7510
Stephan Aßmus <stephan.assmus@sap.com>
2023-11-15 11:34:15 +0100

    Improved rendering of messages
    - split paragraphs
    - substitute **...** -> <strong>...</strong>
    - substitute *...* -> <i>...</i>

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import {Rings} from "react-loader-spinner";
+import PersonIcon from '@mui/icons-material/Person';
+import AssistantIcon from '@mui/icons-material/Assistant';
 
 export function MessageCard({role, content}: Props) {
   const icon = role == "user" ? "person-placeholder" : "sys-monitor"
   const bgClass = role == "user" ? "bg-container" : ""
   return (
     <div className={`w-full ${bgClass} flex border-b border-border-header py-4`}>
-      <div className={`icon-${icon} pl-28 chat-source`}/>
+      {role == "user" ? <PersonIcon color="disabled"/> : <AssistantIcon color="disabled"/>}
       <div className="ml-6 pr-28">
-        {role == "agent" && !content ?
+        {role == "assistant" && content === "" ?
           <Rings
             height="30"
             width="30"
@@ -19,10 +21,53 @@ export function MessageCard({role, content}: Props) {
             visible={true}
             ariaLabel="rings-loading"
           />
-          : content}
+          : <div dangerouslySetInnerHTML={toInnerHtml(content)}/>}
       </div>
     </div>
   );
+}
+
+function replaceAsterisks(str) {
+  let replaceWithItalic = true;
+  return str.replace(/\*/g, () => {
+    if (replaceWithItalic) {
+      replaceWithItalic = false;
+      return '<i>';
+    } else {
+      replaceWithItalic = true;
+      return '</i>';
+    }
+  });
+}
+
+function replaceDoubleAsterisks(str) {
+  let replaceWithStrong = true;
+  return str.replace(/\*\*/g, () => {
+    if (replaceWithStrong) {
+      replaceWithStrong = false;
+      return '<strong>';
+    } else {
+      replaceWithStrong = true;
+      return '</strong>';
+    }
+  });
+}
+
+function removeHTMLTags(str) {
+  return str.replace(/<.*?>/g, "");
+}
+
+function wrapParagraphs(str) {
+  // Wrap in <p> tags
+  return str.replace(/(.+)(\n|$)/g, "<p>$1</p>");
+}
+
+function toInnerHtml(content: string) {
+  content = removeHTMLTags(content);
+  content = replaceDoubleAsterisks(content);
+  content = replaceAsterisks(content);
+  content = wrapParagraphs(content);
+  return {__html: content}
 }
 
 interface Props {
