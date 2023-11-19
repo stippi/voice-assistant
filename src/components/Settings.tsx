a5c9a9e7ef27f0a0bd3449623731cd14865a1895
Stephan Aßmus <stephan.assmus@sap.com>
2023-11-19 23:53:23 +0100

    Use PopOver for Settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -5,7 +5,7 @@ import {
   FormControlLabel,
   IconButton,
   InputLabel,
-  MenuItem,
+  MenuItem, Popover,
   Select,
   Slider,
   Stack,
@@ -35,89 +35,113 @@ export function Settings({}: Props) {
   
   const { settings, setSettings } = useSettings();
   
-  const [open, setOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null | undefined>(null);
+  
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
   
   const voices = ["Alloy", "Echo", "Fable", "Onyx", "Nova", "Shimmer"];
   const personalities = ["Curious", "Professional", "Friendly", "Peppy", "Snarky", "Silly", "Zen"];
   
   return (
-    <div className="fixedTopRight">
+    <div className={`fixedTopRight ${open ? 'open' : ''}`}>
       <div className="settingsContainer">
-        {open && (<div className="settingsMenu">
-          <FormControl>
-            <FormControlLabel
-              checked={settings.openMic}
-              control={<Switch color="primary" />}
-              label="Open Mic"
-              labelPlacement="end"
-              onChange={() => setSettings({ ...settings, openMic: !settings.openMic })}
-            />
-          </FormControl>
-          <FormControl>
-            <FormControlLabel
-              checked={settings.useWhisper}
-              control={<Switch color="primary" />}
-              label="Use Whipser Transcription"
-              labelPlacement="end"
-              onChange={() => setSettings({ ...settings, useWhisper: !settings.useWhisper })}
-            />
-          </FormControl>
-          <TextField
-            label="Trigger phrase"
-            value={settings.triggerPhrase}
-            onChange={(event) => setSettings({ ...settings, triggerPhrase: event.target.value })}
-            helperText="Use something reliably recognized"
-            variant="filled"
-          />
-          <FormControl sx={{ m: 1, minWidth: 120, margin: 0 }} size="small">
-            <InputLabel>Personality</InputLabel>
-            <Select
-              value={settings.personality}
-              label="Personality"
-              onChange={(event) => {setSettings({ ...settings, personality: event.target.value as Personality });}}
-            >
-              {personalities.map((personality, index) => (
-                <MenuItem key={index} value={personality.toLowerCase()}>{personality}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl sx={{ m: 1, minWidth: 120, margin: 0 }} size="small">
-            <InputLabel>Voice</InputLabel>
-            <Select
-              value={settings.voice}
-              label="Voice"
-              onChange={(event) => {setSettings({ ...settings, voice: event.target.value as Voice });}}
-            >
-              {voices.map((voice, index) => (
-                <MenuItem key={index} value={voice.toLowerCase()}>{voice}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-            <SpeedIcon/>
-            <Slider
-              aria-label="Custom marks"
-              value={settings.audioSpeed}
-              onChange={(event, newValue) => setSettings({ ...settings, audioSpeed: newValue as number })}
-              min={0.25}
-              max={2.0}
-              step={0.05}
-              valueLabelDisplay="auto"
-              marks={audioSpeedMarks}
-            />
-          </Stack>
-        </div>)
-        }
         <IconButton
-          className="sendButton"
-          aria-label="send message"
-          onMouseDown={(event) => {
-            event.preventDefault();
-            setOpen(!open);
-          }}
+          aria-label="open settings"
+          aria-describedby={id}
+          onMouseDown={handleClick}
         >
           <SettingsIcon />
         </IconButton>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          disableScrollLock={true}
+        >
+          <div className="settingsMenu">
+            <FormControl>
+              <FormControlLabel
+                checked={settings.openMic}
+                control={<Switch color="primary" />}
+                label="Open Mic"
+                labelPlacement="end"
+                onChange={() => setSettings({ ...settings, openMic: !settings.openMic })}
+              />
+            </FormControl>
+            <FormControl>
+              <FormControlLabel
+                checked={settings.useWhisper}
+                control={<Switch color="primary" />}
+                label="Use Whipser Transcription"
+                labelPlacement="end"
+                onChange={() => setSettings({ ...settings, useWhisper: !settings.useWhisper })}
+              />
+            </FormControl>
+            <TextField
+              label="Trigger phrase"
+              value={settings.triggerPhrase}
+              onChange={(event) => setSettings({ ...settings, triggerPhrase: event.target.value })}
+              helperText="Use something reliably recognized"
+              variant="filled"
+            />
+            <FormControl sx={{ m: 1, minWidth: 120, margin: 0 }} size="small">
+              <InputLabel>Personality</InputLabel>
+              <Select
+                value={settings.personality}
+                label="Personality"
+                onChange={(event) => {setSettings({ ...settings, personality: event.target.value as Personality });}}
+              >
+                {personalities.map((personality, index) => (
+                  <MenuItem key={index} value={personality.toLowerCase()}>{personality}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl sx={{ m: 1, minWidth: 120, margin: 0 }} size="small">
+              <InputLabel>Voice</InputLabel>
+              <Select
+                value={settings.voice}
+                label="Voice"
+                onChange={(event) => {setSettings({ ...settings, voice: event.target.value as Voice });}}
+              >
+                {voices.map((voice, index) => (
+                  <MenuItem key={index} value={voice.toLowerCase()}>{voice}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+              <SpeedIcon/>
+              <Slider
+                aria-label="Custom marks"
+                value={settings.audioSpeed}
+                onChange={(event, newValue) => setSettings({ ...settings, audioSpeed: newValue as number })}
+                min={0.25}
+                max={2.0}
+                step={0.05}
+                valueLabelDisplay="auto"
+                marks={audioSpeedMarks}
+              />
+            </Stack>
+          </div>
+        </Popover>
       </div>
     </div>
   );
