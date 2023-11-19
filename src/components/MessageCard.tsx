bcf5e23c0d1eda5f6d4bdf3ab235ce68b1ec5309
Stephan Aßmus <stephan.assmus@sap.com>
2023-11-19 23:54:09 +0100

    Remove dead code, reduce code size

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -6,11 +6,7 @@ import PersonIcon from '@mui/icons-material/Person';
 import AssistantIcon from '@mui/icons-material/Assistant'
 import Markdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { materialLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { nnfx } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export function MessageCard({role, content, className, ref}: Props) {
   // const user = "😀";
@@ -34,7 +30,6 @@ export function MessageCard({role, content, className, ref}: Props) {
             visible={true}
             ariaLabel="rings-loading"
           />
-          //: <div dangerouslySetInnerHTML={toInnerHtml(content)}/>
           : <Markdown
               children={content}
               components={{
@@ -47,6 +42,7 @@ export function MessageCard({role, content, className, ref}: Props) {
                       children={String(children).replace(/\n$/, '')}
                       language={match[1]}
                       style={oneLight}
+                      customStyle={{fontSize: "15px"}}
                     />
                   ) : (
                     <code {...rest} className={className}>
@@ -67,86 +63,4 @@ interface Props {
   content: string
   className: string
   ref?: React.Ref<HTMLDivElement>
-}
-
-function replaceAsterisks(str) {
-  let replaceWithItalic = true;
-  return str.replace(/\*/g, () => {
-    if (replaceWithItalic) {
-      replaceWithItalic = false;
-      return '<i>';
-    } else {
-      replaceWithItalic = true;
-      return '</i>';
-    }
-  });
-}
-
-function replaceDoubleAsterisks(str) {
-  let replaceWithStrong = true;
-  return str.replace(/\*\*/g, () => {
-    if (replaceWithStrong) {
-      replaceWithStrong = false;
-      return '<strong>';
-    } else {
-      replaceWithStrong = true;
-      return '</strong>';
-    }
-  });
-}
-
-function replaceBlocks(str) {
-  let opening = true;
-  return str.replace(/```/g, () => {
-    if (opening) {
-      opening = false;
-      return '<pre><code>';
-    } else {
-      opening = true;
-      return '</code></pre>';
-    }
-  });
-}
-
-function removeHTMLTags(str) {
-  return str.replace(/<.*?>/g, "");
-}
-
-function wrapParagraphs(str) {
-  const lines = str.split('\n');
-  let html = '';
-  let inList = false;
-  
-  for (const line of lines) {
-    const listMatch = line.match(/^(\d+)\. (.*)/);
-    
-    if (listMatch) {
-      if (!inList) {
-        html += '<ol style="list-style: none; padding-left: 0;">';
-        inList = true;
-      }
-      html += `<li><span class="list-number">${listMatch[1]}.</span> ${listMatch[2]}</li>`;
-    } else {
-      if (inList) {
-        html += '</ol>';
-        inList = false;
-      }
-      html += line.length > 0 ? `<p>${line}</p>` : '';
-    }
-  }
-  
-  if (inList) {
-    html += '</ol>';
-  }
-  
-  return html;
-}
-
-function toInnerHtml(content: string) {
-  content = removeHTMLTags(content);
-  content = replaceDoubleAsterisks(content);
-  content = replaceAsterisks(content);
-  content = replaceBlocks(content);
-  content = wrapParagraphs(content);
-  return {__html: content}
 }
