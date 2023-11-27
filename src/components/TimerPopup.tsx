5b6027e1bd132dcc63392d182092a3ccd9f45824
Stephan Aßmus <stephan.assmus@sap.com>
2023-11-27 23:30:23 +0100

    Play sound when activated, improve timers

--- src/components/TimerPopup.tsx
+++ src/components/TimerPopup.tsx
@@ -1,37 +1,11 @@
 import React from "react";
 import './TimerPopup.css';
+import AvTimerIcon from '@mui/icons-material/AvTimer';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import {Timer} from "../model/timer";
+import {calculateTimeLeft, formatDateRelativeToToday} from "../utils/timeFormat.ts";
 
-import {Timer} from "../contexts/TimersContext";
-
-type TimeLeft = {
-  difference: number;
-  hours: number;
-  minutes: number;
-  seconds: number;
-}
-
-const calculateTimeLeft = (targetTime: string | number | Date): TimeLeft => {
-  const difference = +new Date(targetTime) - +new Date();
-  let timeLeft: TimeLeft = {
-    difference: difference,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  };
-  
-  if (difference > 0) {
-    timeLeft = {
-      difference: difference,
-      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((difference / 1000 / 60) % 60),
-      seconds: Math.floor((difference / 1000) % 60),
-    };
-  }
-  
-  return timeLeft;
-};
-
-const padWithZero = (n: number) => {
+function padWithZero(n: number) {
   return n < 10 ? `0${n}` : `${n}`;
 }
 
@@ -53,9 +27,16 @@ export const TimerPopup = ({ timer }: Props) => {
   
   return (
     <div className="timer">
-      <div className="name">{timer.title}</div>
-      <div className="time">
-        {timeLeft.hours}:{padWithZero(timeLeft.minutes)}:{padWithZero(timeLeft.seconds)}
+      <div className="timer-icon">
+        {timer.type === "countdown" ? <AvTimerIcon /> : <AlarmIcon />}
+      </div>
+      <div className="timer-info">
+        <div className="timer-name">{timer.title}</div>
+        <div className="timer-time">
+          {timer.type === "countdown" ?
+            `${timeLeft.hours}:${padWithZero(timeLeft.minutes)}:${padWithZero(timeLeft.seconds)}`
+            : formatDateRelativeToToday(timer.time)}
+        </div>
       </div>
     </div>
   );
