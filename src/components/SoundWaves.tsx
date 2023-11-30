6fbdfc37cd683383cd943c3973dad34c4e106e4b
Stephan Aßmus <stephan.assmus@sap.com>
2023-11-30 22:17:23 +0100

    Work some more on timers, now with remove button

---
+++ src/components/SoundWaves.tsx
@@ -0,0 +1,11 @@
+import './SoundWaves.css'
+
+export function SoundWaves() {
+  return (
+    <div className="sound-wave-container">
+      <div className="sound-wave"></div>
+      <div className="sound-wave"></div>
+      <div className="sound-wave"></div>
+    </div>
+  );
+}
