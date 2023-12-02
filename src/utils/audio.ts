932c35d6f14a39a66624043eb22fc0cf2fdd058f
Stephan Aßmus <stephan.assmus@gmx.de>
2023-12-02 15:53:19 +0100

    Timers now actually beep

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -3,10 +3,14 @@ const sounds = {
   activation: {
     file: "activation-beep-01.wav",
     audio: null as HTMLAudioElement | null
+  },
+  alarm: {
+    file: "alarm-beep-01.wav",
+    audio: null as HTMLAudioElement | null
   }
 };
 
-export function playSound(sound: keyof typeof sounds) {
+export function playSound(sound: keyof typeof sounds): HTMLAudioElement {
   if (sounds[sound] === undefined) {
     throw new Error(`Unknown sound ${sound}`);
   }
@@ -18,4 +22,5 @@ export function playSound(sound: keyof typeof sounds) {
   entry.audio.play().catch(() => {
     console.log(`Failed to play sound ${sound}`);
   });
+  return entry.audio;
 }
