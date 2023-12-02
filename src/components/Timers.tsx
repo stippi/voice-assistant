223869ceeafcce79f856b1249e4cf5e6e1547b8c
Stephan Aßmus <stephan.assmus@sap.com>
2023-12-02 15:53:19 +0100

    Timers now actually beep

--- src/components/Timers.tsx
+++ src/components/Timers.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import './Timers.css'
 import useTimers from "../hooks/useTimers";
 import {TimerPopup} from "./TimerPopup";
+import {playSound} from "../utils/audio.ts";
 
 function isSameSecond(date1: Date, date2: Date): boolean {
   date1.setMilliseconds(0);
@@ -14,7 +15,7 @@ export function Timers() {
   const { timers, setTimers } = useTimers();
   
   React.useEffect(() => {
-    const interval = setInterval(() => {
+    const interval = window.setInterval(() => {
       if (timers.length === 0) {
         return;
       }
@@ -22,12 +23,39 @@ export function Timers() {
       const updatedTimers = timers
         .map(timer => isSameSecond(new Date(timer.time), now) ? {...timer, ringing: true} : timer)
         .filter(timer => timer.ringing === true || new Date(timer.time) > now);
-      if (!updatedTimers.every((timer, index) => timer.ringing === timers[index].ringing)) {
+      if (timers.length != updatedTimers.length ||
+        !timers.every((timer, index) => timer.ringing === updatedTimers[index].ringing)) {
         setTimers(updatedTimers);
       }
     }, 1000);
     
-    return () => clearInterval(interval);
+    let timeout = -1;
+    if (timers.length > 0) {
+      const nextDate = timers
+        .map(timer => new Date(timer.time))
+        .reduce((minDate, date) => {
+          return date < minDate ? date : minDate;
+        });
+      const now = new Date();
+      const waitTime = nextDate.getTime() - now.getTime();
+      if (waitTime >= 0) {
+        timeout = window.setTimeout(() => {
+          const audio = playSound("alarm");
+          audio.addEventListener("ended", () => {
+            // audio.currentTime = 0;
+            // audio.play().catch((error) => {
+            //   console.log("Failed to play sound (repeat: ", error);
+            // });
+            audio.remove();
+            setTimers(timers.map(timer => ({...timer, ringing: false})));
+          });
+        }, waitTime);
+      }
+    }
+    return () => {
+      window.clearInterval(interval);
+      window.clearTimeout(timeout);
+    }
   }, [timers, setTimers]);
   
   return (
