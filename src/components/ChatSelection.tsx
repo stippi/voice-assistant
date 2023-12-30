8e2b2d2a4d849c9521ae49c815a127147e7756dc
Stephan Aßmus <stephan.assmus@gmx.de>
2023-12-30 17:47:32 +0100

    Layout accounts for sidebar, can be collapsed

--- src/components/ChatSelection.tsx
+++ src/components/ChatSelection.tsx
@@ -1,9 +1,21 @@
-//import React from "react";
+import React from "react";
 import './ChatSelection.css'
 import useChats from "../hooks/useChats";
 
 export function ChatSelection() {
   const { chats, setCurrentChat, newChat } = useChats();
+  const [expanded, setExpanded] = React.useState(true);
+  
+  React.useEffect(() => {
+    const root = document.documentElement;
+    if (expanded) {
+      console.log("expanded");
+      root.style.setProperty('--chat-selection-width', '12vw');
+    } else {
+      console.log("collapsed");
+      root.style.setProperty('--chat-selection-width', '5vw');
+    }
+  }, [expanded]);
   
   const sortedChats = chats.sort((a, b) => {
     return b.lastUpdated - a.lastUpdated;
@@ -11,6 +23,12 @@ export function ChatSelection() {
   
   return (
     <div className="chat-selection">
+      <div
+        onClick={() => setExpanded(!expanded)}
+      >
+        X
+      </div>
+      
       <div
         className="chat-item"
         onClick={() => newChat([])}
