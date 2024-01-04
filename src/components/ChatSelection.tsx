3a97cbafa8ba14c247a004bdf95f67ad1343fbc2
Stephan Aßmus <stephan.assmus@sap.com>
2024-01-04 18:02:08 +0100

    Restore chats if necessary

--- src/components/ChatSelection.tsx
+++ src/components/ChatSelection.tsx
@@ -1,8 +1,10 @@
 import './ChatSelection.css'
 import useChats from "../hooks/useChats";
+import {IconButton} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export function ChatSelection() {
-  const { chats, setCurrentChat, currentChatID} = useChats();
+  const { chats, setCurrentChat, currentChatID, deleteChat} = useChats();
   
   const sortedChats = chats.sort((a, b) => {
     return b.lastUpdated - a.lastUpdated;
@@ -17,6 +19,11 @@ export function ChatSelection() {
           onClick={() => setCurrentChat(chat.id)}
         >
           {new Date(chat.lastUpdated).toLocaleString()}
+          {chat.id === currentChatID && (<div className="chat-remove">
+            <IconButton aria-label="delete" size="small" onClick={() => {deleteChat(chat.id)}}>
+              <DeleteIcon fontSize="inherit" sx={{color: "#999"}}/>
+            </IconButton>
+          </div>)}
         </div>
       ))}
     </div>
