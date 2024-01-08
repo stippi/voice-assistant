6f81dd0740a985c37f38a180c640f95cde1dfddc
Stephan Aßmus <stephan.assmus@gmx.de>
2024-01-08 08:20:14 +0100

    WIP: Rename chat. Show bin icon only on hover

--- src/components/ChatSelection.tsx
+++ src/components/ChatSelection.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import './ChatSelection.css'
 import useChats from "../hooks/useChats";
 import {IconButton} from "@mui/material";
@@ -5,6 +6,15 @@ import DeleteIcon from "@mui/icons-material/Delete";
 
 export function ChatSelection() {
   const { chats, setCurrentChat, currentChatID, deleteChat} = useChats();
+  const [ hoveredChat, setHoveredChat ] = React.useState("");
+  
+  const handleMouseEnter = (chatID: string) => {
+    setHoveredChat(chatID);
+  };
+  
+  const handleMouseLeave = () => {
+    setHoveredChat("");
+  };
   
   const sortedChats = chats.sort((a, b) => {
     return b.lastUpdated - a.lastUpdated;
@@ -16,10 +26,12 @@ export function ChatSelection() {
         <div
           key={chat.id}
           className={`chat-item${chat.id === currentChatID ? ' active-chat-item' : ''}`}
+          onMouseEnter={() => handleMouseEnter(chat.id)}
+          onMouseLeave={handleMouseLeave}
           onClick={() => setCurrentChat(chat.id)}
         >
-          {new Date(chat.lastUpdated).toLocaleString()}
-          {chat.id === currentChatID && (<div className="chat-remove">
+          {chat.name || new Date(chat.lastUpdated).toLocaleString()}
+          {chat.id === currentChatID && chat.id === hoveredChat && (<div className="chat-remove">
             <IconButton aria-label="delete" size="small" onClick={() => {deleteChat(chat.id)}}>
               <DeleteIcon fontSize="inherit" sx={{color: "#999"}}/>
             </IconButton>
