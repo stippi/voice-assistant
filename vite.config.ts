d8e05a8f03fab0a65c54fe099385129ac58621e1
Stephan Aßmus <stephan.assmus@sap.com>
2024-01-18 22:27:06 +0100

    Add access to Google Places API via tool

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // The proxy is used to avoid CORS issues when calling the Google Places API
+    // The response does not include the Access-Control-Allow-Origin header.
+    proxy: {
+      '/place-api': {
+        target: 'https://places.googleapis.com/v1/places:searchText',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/place-api/, '')
+      }
+    }
+  }
 })
