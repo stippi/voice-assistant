772342394760d07c1bfd9af6def2f4ab4733e1d1
Stephan Aßmus <superstippi@gmx.de>
2024-01-19 09:58:00 +0100

    Formatting

--- src/components/VoiceAssistant.tsx
+++ src/components/VoiceAssistant.tsx
@@ -342,7 +342,7 @@ export default function VoiceAssistant() {
     responseCancelledRef.current = true;
   }, []);
   
-  const { windowFocused} = useWindowFocus();
+  const {windowFocused} = useWindowFocus();
   useEffect(() => {
     if (windowFocused) {
       document.body.classList.add('window-focused');
