2fa21d3e4d27068a635dd4c0707da035087ba253
Stephan Aßmus <stephan.assmus@sap.com>
2024-02-19 09:27:27 +0100

    Include more properties in Message type

--- src/model/message.ts
+++ src/model/message.ts
@@ -4,5 +4,7 @@ import ChatCompletionMessageToolCall = OpenAI.ChatCompletionMessageToolCall;
 export type Message = {
   role: "user" | "assistant" | "system" | "tool",
   content: string | null,
+  name?: string,
+  tool_call_id?: string,
   tool_calls?: ChatCompletionMessageToolCall[],
 }
