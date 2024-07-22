da416d5319339de02da623b054d1692b5bb1ba5f
Stephan Aßmus <stephan.assmus@sap.com>
2024-07-22 14:06:21 +0200

    Make EditableListItem use React.forwardRef

--- packages/frontend/src/components/common/EditableListItem.tsx
+++ packages/frontend/src/components/common/EditableListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, forwardRef } from "react";
 import { Checkbox, ListItem, ListItemIcon, ListItemText, IconButton, Input, ListItemButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import DoneIcon from "@mui/icons-material/Done";
@@ -22,122 +22,119 @@ interface ItemProps {
   isActive?: boolean;
 }
 
-const EditableListItem = ({
-  item,
-  fallbackName,
-  onClick,
-  onRename,
-  onDuplicate,
-  onDelete,
-  isSelected,
-  onActivate,
-  isActive,
-}: ItemProps) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [currentName, setCurrentName] = useState(item.name);
-  const [hovered, setHovered] = useState(false);
-  const onMouseEnter = () => {
-    setHovered(true);
-  };
-  const onMouseLeave = () => {
-    setHovered(false);
-  };
+const EditableListItem = forwardRef<HTMLLIElement, ItemProps & React.HTMLAttributes<HTMLLIElement>>(
+  (
+    { item, fallbackName, onClick, onRename, onDuplicate, onDelete, isSelected, onActivate, isActive, ...props },
+    ref,
+  ) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [currentName, setCurrentName] = useState(item.name);
+    const [hovered, setHovered] = useState(false);
+    const onMouseEnter = () => {
+      setHovered(true);
+    };
+    const onMouseLeave = () => {
+      setHovered(false);
+    };
 
-  useEffect(() => {
-    if (!isSelected) {
+    useEffect(() => {
+      if (!isSelected) {
+        setIsEditing(false);
+      }
+    }, [isSelected]);
+
+    const doneRenaming = () => {
+      onRename(currentName);
       setIsEditing(false);
-    }
-  }, [isSelected]);
+    };
 
-  const doneRenaming = () => {
-    onRename(currentName);
-    setIsEditing(false);
-  };
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        doneRenaming();
+      }
+    };
 
-  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      doneRenaming();
-    }
-  };
-
-  return (
-    <ListItem
-      disablePadding
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      secondaryAction={
-        isEditing ? (
-          <IconButton edge="end" aria-label="done" size="small" onClick={doneRenaming}>
-            <DoneIcon fontSize="inherit" />
-          </IconButton>
-        ) : isSelected ? (
-          <div
-            style={{
-              opacity: hovered ? 1 : 0,
-              transition: "opacity 0.2s ease-in-out",
-              backgroundColor: "inherit",
-            }}
-          >
-            <IconButton edge="end" aria-label="rename" size="small" onClick={() => setIsEditing(true)}>
-              <EditIcon fontSize="inherit" />
+    return (
+      <ListItem
+        ref={ref}
+        disablePadding
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        secondaryAction={
+          isEditing ? (
+            <IconButton edge="end" aria-label="done" size="small" onClick={doneRenaming}>
+              <DoneIcon fontSize="inherit" />
             </IconButton>
-            {onDuplicate && (
-              <IconButton edge="end" aria-label="duplicate" size="small" onClick={onDuplicate}>
-                <IoDuplicate fontSize="inherit" />
-              </IconButton>
-            )}
-            <IconButton
-              edge="end"
-              aria-label="delete"
-              size="small"
-              onClick={onDelete}
+          ) : isSelected ? (
+            <div
               style={{
                 opacity: hovered ? 1 : 0,
                 transition: "opacity 0.2s ease-in-out",
+                backgroundColor: "inherit",
               }}
             >
-              <DeleteIcon fontSize="inherit" />
-            </IconButton>
-          </div>
-        ) : (
-          <></>
-        )
-      }
-    >
-      {isEditing ? (
-        <Input
-          value={currentName}
-          onChange={(e) => setCurrentName(e.target.value)}
-          autoFocus
-          fullWidth
-          onKeyDown={onKeyDown}
-          sx={{
-            paddingLeft: "1rem",
-            paddingRight: "1rem",
-          }}
-        />
-      ) : (
-        <ListItemButton disableRipple selected={isSelected} onClick={onClick}>
-          {onActivate && (
-            <ListItemIcon sx={{ minWidth: 0 }}>
-              <Checkbox edge="start" checked={isActive} onClick={onActivate} tabIndex={-1} disableRipple />
-            </ListItemIcon>
-          )}
-          <ListItemText
-            id={item.id}
-            primary={item.name || fallbackName}
-            primaryTypographyProps={{
-              fontSize: 14,
-              fontWeight: "medium",
-              whiteSpace: "nowrap",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
+              <IconButton edge="end" aria-label="rename" size="small" onClick={() => setIsEditing(true)}>
+                <EditIcon fontSize="inherit" />
+              </IconButton>
+              {onDuplicate && (
+                <IconButton edge="end" aria-label="duplicate" size="small" onClick={onDuplicate}>
+                  <IoDuplicate fontSize="inherit" />
+                </IconButton>
+              )}
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                size="small"
+                onClick={onDelete}
+                style={{
+                  opacity: hovered ? 1 : 0,
+                  transition: "opacity 0.2s ease-in-out",
+                }}
+              >
+                <DeleteIcon fontSize="inherit" />
+              </IconButton>
+            </div>
+          ) : (
+            <></>
+          )
+        }
+        {...props}
+      >
+        {isEditing ? (
+          <Input
+            value={currentName}
+            onChange={(e) => setCurrentName(e.target.value)}
+            autoFocus
+            fullWidth
+            onKeyDown={onKeyDown}
+            sx={{
+              paddingLeft: "1rem",
+              paddingRight: "1rem",
             }}
           />
-        </ListItemButton>
-      )}
-    </ListItem>
-  );
-};
+        ) : (
+          <ListItemButton disableRipple selected={isSelected} onClick={onClick}>
+            {onActivate && (
+              <ListItemIcon sx={{ minWidth: 0 }}>
+                <Checkbox edge="start" checked={isActive} onClick={onActivate} tabIndex={-1} disableRipple />
+              </ListItemIcon>
+            )}
+            <ListItemText
+              id={item.id}
+              primary={item.name || fallbackName}
+              primaryTypographyProps={{
+                fontSize: 14,
+                fontWeight: "medium",
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+              }}
+            />
+          </ListItemButton>
+        )}
+      </ListItem>
+    );
+  },
+);
 
 export default EditableListItem;
