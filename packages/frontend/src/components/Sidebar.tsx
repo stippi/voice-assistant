8251e64ef53f8e77722e765e86e6ccfa4ec69cd5
Stephan Aßmus <stephan.assmus@sap.com>
2024-07-25 23:29:41 +0200

    Prettier, download button as icon
    Placement is still ugly

--- packages/frontend/src/components/Sidebar.tsx
+++ packages/frontend/src/components/Sidebar.tsx
@@ -1,44 +1,50 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import {styled, createTheme, ThemeProvider} from '@mui/material/styles';
-import Divider from '@mui/material/Divider';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import ArrowRight from '@mui/icons-material/ArrowRight';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import Settings from '@mui/icons-material/Settings';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import ArrowRight from "@mui/icons-material/ArrowRight";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Settings from "@mui/icons-material/Settings";
 import useChats from "../hooks/useChats";
-import './Sidebar.css'
-import {ChatSelection} from "./ChatSelection";
-import {AssistantSettings} from "./AssistantSettings";
-import {RiRobot2Fill} from "react-icons/ri";
-import {Button} from "@mui/material";
+import "./Sidebar.css";
+import { ChatSelection } from "./ChatSelection";
+import { AssistantSettings } from "./AssistantSettings";
+import { RiRobot2Fill } from "react-icons/ri";
+import { Button } from "@mui/material";
+import SaveAltIcon from "@mui/icons-material/SaveAlt";
 
 const SidebarList = styled(List)<{ component?: React.ElementType }>({
-  '& .MuiListItemButton-root': {
+  "& .MuiListItemButton-root": {
     paddingLeft: 16,
     paddingRight: 16,
   },
-  '& .MuiListItemIcon-root': {
+  "& .MuiListItemIcon-root": {
     minWidth: 0,
     marginRight: 12,
   },
-  '& .MuiSvgIcon-root': {
+  "& .MuiSvgIcon-root": {
     fontSize: 20,
   },
 });
 
 const DownloadChatsButton: React.FC = () => {
   const { downloadChats } = useChats();
-  return <Button onClick={downloadChats}>Download chats</Button>;
+  return (
+    <Tooltip title="Download all chats">
+      <IconButton onClick={downloadChats} sx={{ fontSize: "1rem" }}>
+        <SaveAltIcon sx={{ fontSize: "inherit" }} />
+      </IconButton>
+    </Tooltip>
+  );
 };
-
 
 const theme = createTheme({
   components: {
@@ -50,79 +56,79 @@ const theme = createTheme({
     MuiFormControlLabel: {
       styleOverrides: {
         label: {
-          fontSize: '14px'
-        }
-      }
+          fontSize: "14px",
+        },
+      },
     },
     MuiFormLabel: {
       styleOverrides: {
         root: {
-          fontSize: '14px'
-        }
-      }
+          fontSize: "14px",
+        },
+      },
     },
     MuiFormHelperText: {
       styleOverrides: {
         root: {
-          fontSize: '11px'
-        }
-      }
+          fontSize: "11px",
+        },
+      },
     },
     MuiInputBase: {
       styleOverrides: {
         root: {
-          paddingLeft: '16px!important'
+          paddingLeft: "16px!important",
         },
         input: {
-          paddingTop: '8px',
-          paddingBottom: '8px',
-          fontSize: '14px'
-        }
-      }
+          paddingTop: "8px",
+          paddingBottom: "8px",
+          fontSize: "14px",
+        },
+      },
     },
     MuiMenuItem: {
       styleOverrides: {
         root: {
-          fontSize: '14px'
-        }
-      }
+          fontSize: "14px",
+        },
+      },
     },
     MuiSlider: {
       styleOverrides: {
         markLabel: {
-          fontSize: '14px'
-        }
-      }
+          fontSize: "14px",
+        },
+      },
     },
   },
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: 'rgb(58,166,255)'
+      main: "rgb(58,166,255)",
     },
     text: {
-      primary: 'rgb(235, 235, 241)',
+      primary: "rgb(235, 235, 241)",
     },
     background: {
-      paper: 'rgb(10, 30, 49)'
+      paper: "rgb(10, 30, 49)",
     },
-  }
+  },
 });
 
 export function Sidebar() {
   const { newChat } = useChats();
-  
+
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
-  
+
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     setAnchorEl(event.currentTarget);
   };
-  
+
   const handleClose = () => {
     setAnchorEl(null);
   };
-  
+
   return (
     <ThemeProvider theme={theme}>
       <Box className="sidebar">
@@ -132,19 +138,19 @@ export function Sidebar() {
             display: "flex",
             flexDirection: "column",
             maxHeight: "100%",
-            overscrollBehavior: "contain"
-        }}
+            overscrollBehavior: "contain",
+          }}
         >
           <SidebarList component="nav" disablePadding>
             <ListItem
               sx={{
-                color: 'rgb(255, 143, 16)',
+                color: "rgb(255, 143, 16)",
               }}
             >
               <ListItemIcon
                 sx={{
-                  color: 'inherit',
-                  fontSize: 20
+                  color: "inherit",
+                  fontSize: 20,
                 }}
               >
                 <RiRobot2Fill />
@@ -152,14 +158,14 @@ export function Sidebar() {
               <ListItemText
                 sx={{
                   my: 0,
-                  textShadow: '0 0 8px rgba(255, 143, 16, 0.2)'
-              }}
+                  textShadow: "0 0 8px rgba(255, 143, 16, 0.2)",
+                }}
                 primary="Voice Assistant"
                 primaryTypographyProps={{
                   fontFamily: "KomikaAxis",
                   fontSize: 18,
-                  fontWeight: 'medium',
-                  paddingBottom: '3px',
+                  fontWeight: "medium",
+                  paddingBottom: "3px",
                   letterSpacing: 0,
                 }}
               />
@@ -173,9 +179,9 @@ export function Sidebar() {
                 <ListItemText
                   primary="New chat"
                   primaryTypographyProps={{
-                    color: 'primary',
-                    fontWeight: 'medium',
-                    variant: 'body2',
+                    color: "primary",
+                    fontWeight: "medium",
+                    variant: "body2",
                   }}
                 />
               </ListItemButton>
@@ -183,43 +189,43 @@ export function Sidebar() {
                 <IconButton
                   size="large"
                   sx={{
-                    '& svg': {
-                      color: 'rgba(255,255,255,0.8)',
-                      transition: '0.2s',
-                      transform: 'translateX(0) rotate(0)',
+                    "& svg": {
+                      color: "rgba(255,255,255,0.8)",
+                      transition: "0.2s",
+                      transform: "translateX(0) rotate(0)",
                     },
-                    '&:hover, &:focus': {
-                      bgcolor: 'unset',
-                      '& svg:first-of-type': {
-                        transform: 'translateX(-4px) rotate(-20deg)',
+                    "&:hover, &:focus": {
+                      bgcolor: "unset",
+                      "& svg:first-of-type": {
+                        transform: "translateX(-4px) rotate(-20deg)",
                       },
-                      '& svg:last-of-type': {
+                      "& svg:last-of-type": {
                         right: 0,
                         opacity: 1,
                       },
                     },
-                    '&::after': {
+                    "&::after": {
                       content: '""',
-                      position: 'absolute',
-                      height: '80%',
-                      display: 'block',
+                      position: "absolute",
+                      height: "80%",
+                      display: "block",
                       left: 0,
-                      width: '1px',
-                      bgcolor: 'divider',
+                      width: "1px",
+                      bgcolor: "divider",
                     },
                   }}
                   onMouseDown={handleClick}
                 >
                   <Settings />
-                  <ArrowRight sx={{ position: 'absolute', right: 4, opacity: 0 }} />
+                  <ArrowRight sx={{ position: "absolute", right: 4, opacity: 0 }} />
                 </IconButton>
               </Tooltip>
-              <AssistantSettings anchorEl={anchorEl} onClose={handleClose}/>
+              <AssistantSettings anchorEl={anchorEl} onClose={handleClose} />
             </ListItem>
             <Divider />
           </SidebarList>
-          <ChatSelection/>
-          <DownloadChatsButton/>
+          <ChatSelection />
+          <DownloadChatsButton />
         </Paper>
       </Box>
     </ThemeProvider>
