5a9e68ec62a3ff18e212ce1287dea23f368430cd
Stephan Aßmus <stephan.assmus@sap.com>
2024-08-14 17:53:06 +0200

    Auto-cleanup

--- packages/frontend/src/components/Dashboard.tsx
+++ packages/frontend/src/components/Dashboard.tsx
@@ -1,119 +1,115 @@
 import React from "react";
-import './Dashboard.css'
+import "./Dashboard.css";
 import Paper from "@mui/material/Paper";
-import {styled, ThemeProvider, createTheme} from "@mui/material/styles";
+import { styled, ThemeProvider, createTheme } from "@mui/material/styles";
 import List from "@mui/material/List";
-import useAppContext from "../hooks/useAppContext.tsx";
-import useGoogleContext from "../hooks/useGoogleContext.tsx";
-import {KeyboardArrowDown} from "@mui/icons-material";
-import AlarmIcon from '@mui/icons-material/Alarm';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import HeadphonesIcon from '@mui/icons-material/Headphones';
-import {Box, ListItemButton, SxProps} from "@mui/material";
+import useAppContext from "../hooks/useAppContext";
+import useGoogleContext from "../hooks/useGoogleContext";
+import { KeyboardArrowDown } from "@mui/icons-material";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import { Box, ListItemButton, SxProps } from "@mui/material";
 import ListItemText from "@mui/material/ListItemText";
-import {Settings} from "../contexts/SettingsContext.tsx";
-import useSettings from "../hooks/useSettings.tsx";
-import {Timers} from "./Timers.tsx";
-import {Events} from "./Events.tsx";
+import { Settings } from "../contexts/SettingsContext";
+import useSettings from "../hooks/useSettings";
+import { Timers } from "./Timers";
+import { Events } from "./Events";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import {CurrentSong, PlaybackControls, PositionControls} from "./MusicControls";
+import { CurrentSong, PlaybackControls, PositionControls } from "./MusicControls";
 import useSpotifyContext from "../hooks/useSpotifyContext";
-import {MusicPlaylist} from "./MusicPlaylist.tsx";
+import { MusicPlaylist } from "./MusicPlaylist";
 import ListItem from "@mui/material/ListItem";
 import IconButton from "@mui/material/IconButton";
 import Divider from "@mui/material/Divider";
-import useMicrosoftContext from "../hooks/useMicrosoftContext.tsx";
-import {Photos} from "./Photos.tsx";
-import {Theme} from "@emotion/react";
-import {gridConfig} from "./dashboardGridConfig.ts";
+import useMicrosoftContext from "../hooks/useMicrosoftContext";
+import { Photos } from "./Photos";
+import { Theme } from "@emotion/react";
+import { gridConfig } from "./dashboardGridConfig";
 
 const DashboardList = styled(List)<{ component?: React.ElementType }>({
-  '& .MuiList-root': {
-    overflow: 'auto'
+  "& .MuiList-root": {
+    overflow: "auto",
   },
-  '& .MuiListItemButton-root': {
+  "& .MuiListItemButton-root": {
     ...gridConfig,
   },
-  '& .MuiListItem-root': {
+  "& .MuiListItem-root": {
     paddingLeft: 16,
     paddingRight: 16,
     paddingTop: 2,
     paddingBottom: 2,
-    display: 'grid',
-    gridTemplateColumns: '32px 1fr 32px',
-    alignItems: 'start',
-    gap: '0 8px',
+    display: "grid",
+    gridTemplateColumns: "32px 1fr 32px",
+    alignItems: "start",
+    gap: "0 8px",
   },
-  '& .MuiListItemIcon-root': {
+  "& .MuiListItemIcon-root": {
     minWidth: 0,
-    justifySelf: 'center',
+    justifySelf: "center",
   },
 });
 
-export function ExpandButton({open, sx, id}: { open: boolean, sx?: SxProps<Theme>, id?: string }) {
-  return <KeyboardArrowDown
-    key={id}
-    sx={{
-      ...sx,
-      mr: -1,
-      color: 'rgba(0,0,0,1)',
-      transform: open ? 'rotate(-180deg)' : 'rotate(0)',
-      transition: '0.2s',
-      fontSize: 20,
-      marginRight: 0,
-      justifySelf: 'center',
-    }}
-  />
+export function ExpandButton({ open, sx, id }: { open: boolean; sx?: SxProps<Theme>; id?: string }) {
+  return (
+    <KeyboardArrowDown
+      key={id}
+      sx={{
+        ...sx,
+        mr: -1,
+        color: "rgba(0,0,0,1)",
+        transform: open ? "rotate(-180deg)" : "rotate(0)",
+        transition: "0.2s",
+        fontSize: 20,
+        marginRight: 0,
+        justifySelf: "center",
+      }}
+    />
+  );
 }
 
-export function CollapsibleList(
-  {
-    header,
-    title,
-    icon,
-    secondaryTitle,
-    settingsKey,
-    children,
-    disableExpand,
-    hideList,
-    sx,
-    expandKey,
-    onMouseEnter,
-    onMouseLeave
-  }: DashboardItemProps
-) {
-  const {settings, setSettings} = useSettings();
+export function CollapsibleList({
+  header,
+  title,
+  icon,
+  secondaryTitle,
+  settingsKey,
+  children,
+  disableExpand,
+  hideList,
+  sx,
+  expandKey,
+  onMouseEnter,
+  onMouseLeave,
+}: DashboardItemProps) {
+  const { settings, setSettings } = useSettings();
   const open = settings[settingsKey];
-  const toggleExpand = () => setSettings({...settings, [settingsKey]: !open});
+  const toggleExpand = () => setSettings({ ...settings, [settingsKey]: !open });
 
   const iconContent = () => {
     if (icon) {
-      return (
-        <ListItemIcon style={{marginTop: 0}}>
-          {icon}
-        </ListItemIcon>
-      );
+      return <ListItemIcon style={{ marginTop: 0 }}>{icon}</ListItemIcon>;
     } else {
       return <div />;
     }
-  }
+  };
   const titleContent = () => {
-    if (typeof title === 'string') {
+    if (typeof title === "string") {
       return (
         <ListItemText
           primary={title}
           primaryTypographyProps={{
             fontSize: 15,
-            fontWeight: 'medium',
-            lineHeight: '20px',
-            mb: '2px',
+            fontWeight: "medium",
+            lineHeight: "20px",
+            mb: "2px",
           }}
           secondary={!header ? secondaryTitle : null}
           secondaryTypographyProps={{
             noWrap: true,
             fontSize: 12,
-            lineHeight: '16px',
-            color: !disableExpand && open ? 'rgba(0,0,0,0)' : 'rgba(0,0,0,0.5)',
+            lineHeight: "16px",
+            color: !disableExpand && open ? "rgba(0,0,0,0)" : "rgba(0,0,0,0.5)",
           }}
           sx={{ my: 0 }}
         />
@@ -121,13 +117,13 @@ export function CollapsibleList(
     } else {
       return title;
     }
-  }
+  };
   const expandContent = () => {
     if (!disableExpand) {
-      return <ExpandButton open={!!open} id={expandKey}/>;
+      return <ExpandButton open={!!open} id={expandKey} />;
     }
-  }
-  
+  };
+
   return (
     <Paper
       elevation={1}
@@ -141,38 +137,40 @@ export function CollapsibleList(
     >
       {header}
       {!hideList && (
-        <DashboardList style={{paddingTop: 0, paddingBottom: 0, width: '100%'}}>
+        <DashboardList style={{ paddingTop: 0, paddingBottom: 0, width: "100%" }}>
           <Box
             sx={{
               paddingTop: 0,
-              paddingBottom: open && !disableExpand ? 1 : 0
+              paddingBottom: open && !disableExpand ? 1 : 0,
             }}
           >
             {disableExpand || typeof title !== "string" ? (
               <ListItem
                 style={{
                   paddingTop: header ? 4 : 16,
-                  paddingBottom: header ? 4 : 16
+                  paddingBottom: header ? 4 : 16,
                 }}
               >
                 {iconContent()}
                 {titleContent()}
                 {!disableExpand && (
-                  <IconButton onClick={toggleExpand} style={{height: 32, width: 32}}>
+                  <IconButton onClick={toggleExpand} style={{ height: 32, width: 32 }}>
                     {expandContent()}
-                  </IconButton>)
-                }
+                  </IconButton>
+                )}
               </ListItem>
             ) : (
               <ListItemButton
                 onMouseDown={(e) => e.preventDefault()}
                 onClick={toggleExpand}
-                sx={{
-                  //'&:hover, &:focus': { '& svg': { opacity: open ? 1 : 0 } },
-                }}
+                sx={
+                  {
+                    //'&:hover, &:focus': { '& svg': { opacity: open ? 1 : 0 } },
+                  }
+                }
                 style={{
                   paddingTop: header ? 4 : 16,
-                  paddingBottom: open ? 0 : header ? 4 : 16
+                  paddingBottom: open ? 0 : header ? 4 : 16,
                 }}
               >
                 {iconContent()}
@@ -208,89 +206,91 @@ const theme = createTheme({
     MuiListItemButton: {
       defaultProps: {
         disableTouchRipple: true,
-      }
-    }
+      },
+    },
   },
   palette: {
-    mode: 'light',
+    mode: "light",
     primary: {
-      main: 'rgb(102, 157, 246)'
+      main: "rgb(102, 157, 246)",
     },
     text: {
-      primary: 'rgb(40, 40, 40)',
+      primary: "rgb(40, 40, 40)",
     },
     background: {
-      paper: 'rgb(235, 235, 235)'
+      paper: "rgb(235, 235, 235)",
     },
-  }
+  },
 });
 
 function MusicList() {
-  const {player, playerState, deviceId, play, pausePlayback, skipNext, skipPrevious} = useSpotifyContext();
+  const { player, playerState, deviceId, play, pausePlayback, skipNext, skipPrevious } = useSpotifyContext();
   return (
     <CollapsibleList
-      header={playerState.trackId && (
-        <Box sx={{paddingLeft: 2, paddingRight: 2, paddingTop: 2}}>
-          <CurrentSong
-            title={playerState.name}
-            artist={playerState.artists.join(", ")}
-            albumTitle={playerState.albumName}
-            albumCoverUrl={playerState.coverImageUrl}
-          />
-          <PositionControls
-            position={playerState.position}
-            duration={playerState.duration}
-            setPosition={async (value: number) => {
-              if (player) {
-                await player.seek(value * 1000);
+      header={
+        playerState.trackId && (
+          <Box sx={{ paddingLeft: 2, paddingRight: 2, paddingTop: 2 }}>
+            <CurrentSong
+              title={playerState.name}
+              artist={playerState.artists.join(", ")}
+              albumTitle={playerState.albumName}
+              albumCoverUrl={playerState.coverImageUrl}
+            />
+            <PositionControls
+              position={playerState.position}
+              duration={playerState.duration}
+              setPosition={async (value: number) => {
+                if (player) {
+                  await player.seek(value * 1000);
+                }
+              }}
+            />
+          </Box>
+        )
+      }
+      icon={playerState.trackId ? <div /> : <HeadphonesIcon style={{ color: "#00ce41", fontSize: "1.5rem" }} />}
+      title={
+        !playerState.trackId ? (
+          "Music"
+        ) : (
+          <PlaybackControls
+            skipPrevious={async () => skipPrevious(deviceId)}
+            skipNext={async () => skipNext(deviceId)}
+            canSkipPrevious={playerState.canSkipPrevious}
+            canSkipNext={playerState.canSkipNext}
+            togglePlay={async () => {
+              if (playerState.paused) {
+                await play(deviceId, []);
+              } else {
+                await pausePlayback(deviceId);
               }
             }}
+            playing={!playerState.paused}
           />
-        </Box>
-      )}
-      icon={playerState.trackId ? (
-        <div />
-      ) : (
-        <HeadphonesIcon style={{color: "#00ce41", fontSize: "1.5rem"}}/>
-      )}
-      title={!playerState.trackId ? "Music" : (
-        <PlaybackControls
-          skipPrevious={async () => skipPrevious(deviceId)}
-          skipNext={async () => skipNext(deviceId)}
-          canSkipPrevious={playerState.canSkipPrevious}
-          canSkipNext={playerState.canSkipNext}
-          togglePlay={async () => {
-            if (playerState.paused) {
-              await play(deviceId, []);
-            } else {
-              await pausePlayback(deviceId);
-            }
-          }}
-          playing={!playerState.paused}
-        />
-      )}
+        )
+      }
       secondaryTitle={playerState.trackId ? "Show playlist" : "No music is currently streaming"}
       settingsKey="showPlaylist"
       disableExpand={!playerState.trackId}
     >
       {playerState.trackId && <Divider />}
-      <MusicPlaylist/>
+      <MusicPlaylist />
     </CollapsibleList>
   );
 }
 
 export function Dashboard() {
-  const {timers, idle} = useAppContext();
-  const {upcomingEvents: upcomingGoogleEvents, favoritePhotos} = useGoogleContext();
-  const {upcomingEvents: upcomingMicrosoftEvents} = useMicrosoftContext();
-  const {settings} = useSettings();
-  
+  const { timers, idle } = useAppContext();
+  const { upcomingEvents: upcomingGoogleEvents, favoritePhotos } = useGoogleContext();
+  const { upcomingEvents: upcomingMicrosoftEvents } = useMicrosoftContext();
+  const { settings } = useSettings();
+
   const upcomingEvents = [];
   if (settings.enableGoogle && settings.enableGoogleCalendar) {
-    upcomingEvents.push(...upcomingGoogleEvents || []);
+    upcomingEvents.push(...(upcomingGoogleEvents || []));
   }
   if (settings.enableMicrosoft) {
-    upcomingEvents.push(...upcomingMicrosoftEvents || []);
+    upcomingEvents.push(...(upcomingMicrosoftEvents || []));
   }
   upcomingEvents.sort((a, b) => {
     const aStart = a.start.dateTime || a.start.date || "";
@@ -299,44 +299,42 @@ export function Dashboard() {
   });
   const hasEvents = upcomingEvents.length > 0;
   const hasPhotos = settings.enableGoogle && settings.enableGooglePhotos && favoritePhotos.length > 0;
-  
+
   React.useEffect(() => {
     const showDashboard = timers.length > 0 || hasEvents || settings.enableSpotify;
-    document.documentElement.style.setProperty('--dashboard-width', showDashboard ? '230px' : '0');
+    document.documentElement.style.setProperty("--dashboard-width", showDashboard ? "230px" : "0");
   }, [timers, hasEvents, settings.enableSpotify]);
-  
+
   return (
     <ThemeProvider theme={theme}>
       <div
         className="dashboard side-column"
         style={{
-          display: timers.length > 0 || hasEvents || settings.enableSpotify || hasPhotos ? "flex" : "none"
+          display: timers.length > 0 || hasEvents || settings.enableSpotify || hasPhotos ? "flex" : "none",
         }}
       >
         {hasEvents && (
           <CollapsibleList
-            icon={<CalendarMonthIcon style={{color: "#00c4ff", fontSize: "1.5rem"}}/>}
+            icon={<CalendarMonthIcon style={{ color: "#00c4ff", fontSize: "1.5rem" }} />}
             title="Calendar"
             secondaryTitle="Show upcoming events"
-            settingsKey="showUpcomingEvents">
-            <Events events={upcomingEvents}/>
+            settingsKey="showUpcomingEvents"
+          >
+            <Events events={upcomingEvents} />
           </CollapsibleList>
         )}
         {timers.length > 0 && (
           <CollapsibleList
-            icon={<AlarmIcon style={{color: "#ff4d17", fontSize: "1.5rem"}}/>}
+            icon={<AlarmIcon style={{ color: "#ff4d17", fontSize: "1.5rem" }} />}
             title="Alarms"
             secondaryTitle="Show alarms and timers"
-            settingsKey="showTimers">
-            <Timers/>
+            settingsKey="showTimers"
+          >
+            <Timers />
           </CollapsibleList>
         )}
-        {settings.enableSpotify && (
-          <MusicList/>
-        )}
-        {hasPhotos && (
-          <Photos idle={idle} mediaItemIDs={favoritePhotos} />
-        )}
+        {settings.enableSpotify && <MusicList />}
+        {hasPhotos && <Photos idle={idle} mediaItemIDs={favoritePhotos} />}
       </div>
     </ThemeProvider>
   );
