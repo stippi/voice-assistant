9443217a4e38a43f24c47168b190c27c7976bc8b
Stephan Aßmus <stephan.assmus@sap.com>
2024-08-31 15:05:05 +0200

    Fetch Google Calendar events from all available calendars

--- packages/shared/src/types/event.ts
+++ packages/shared/src/types/event.ts
@@ -1,12 +1,11 @@
-
 export interface EventTime {
   // The date, in the format "yyyy-mm-dd", if this is an all-day event.
   date?: string | undefined;
-  
+
   // The time, as a combined date-time value (formatted according to RFC3339).
   // A time zone offset is required unless a time zone is explicitly specified in timeZone.
   dateTime?: string | undefined;
-  
+
   // The time zone in which the time is specified. (Formatted as an IANA Time Zone Database name, e.g. "Europe/Zurich".)
   // For recurring events this field is required and specifies the time zone in which the recurrence is expanded.
   // For single events this field is optional and indicates a custom time zone for the event start/end.
@@ -17,7 +16,7 @@ export interface CalendarEvent {
   id: string;
   // The (inclusive) start time of the event. For a recurring event, this is the start time of the first instance.
   start: EventTime;
-  
+
   // The (exclusive) end time of the event. For a recurring event, this is the end time of the first instance.
   end: EventTime;
 
@@ -25,4 +24,5 @@ export interface CalendarEvent {
   summary: string;
   location?: string;
   htmlLink?: string;
+  calendarId: string;
 }
