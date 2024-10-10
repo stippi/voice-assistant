18bc9a51d77035a151579958ebc66544aa97046e
Stephan Aßmus <stephan.assmus@gmx.de>
2024-10-10 23:57:08 +0200

    Various improvements to the conversation flow
    But still, the audio from the assistant might still be streaming,
    because we receive the audio faster than realtime. We need to block
    the mic until the audio finished for that assistant item.

--- packages/frontend/src/components/RealTimeAssistant.tsx
+++ packages/frontend/src/components/RealTimeAssistant.tsx
@@ -3,6 +3,7 @@ import "./MessageBar.css";
 import IconButton from "@mui/material/IconButton";
 import MicIcon from "@mui/icons-material/Mic";
 import RecordVoiceOverIcon from "@mui/icons-material/RecordVoiceOver";
+import { RiRobot2Fill } from "react-icons/ri";
 import { RealtimeClient } from "@openai/realtime-api-beta";
 import { ItemType, ToolDefinitionType } from "@openai/realtime-api-beta/dist/lib/client.js";
 import { completionsApiKey } from "../config";
@@ -13,7 +14,8 @@ import { createSystemMessageService } from "../services/SystemMessageService";
 import { Message } from "@shared/types";
 import { Conversation } from "./chat/Conversation";
 import { getTools, callFunction } from "../integrations/tools";
-import { useAppContext, useSettings, useTimers, useSpotifyContext } from "../hooks";
+import { useAppContext, useSettings, useTimers } from "../hooks";
+import { Settings } from "../contexts/SettingsContext";
 // import { Conversation } from "./chat/Conversation";
 // import { MessageBar } from "./MessageBar";
 // import { useVoiceAssistant } from "../hooks";
@@ -23,12 +25,21 @@ type RealtimeEvent = {
   delta?: { audio: Int16Array | Int16Array };
 };
 
+type ClientEvent = {
+  time: string;
+  source: "client" | "server";
+  event: {
+    type: string;
+  };
+};
+
 export default function RealtimeAssistant() {
   const wavStreamPlayerRef = useRef(new AudioStreamingService({ sampleRate: 24000 }));
   const clientRef = useRef(
     new RealtimeClient({
       apiKey: completionsApiKey,
       dangerouslyAllowAPIKeyInBrowser: true,
+      //p      debug: true,
     }),
   );
   const systemMessageServiceRef = useRef(createSystemMessageService());
@@ -40,38 +51,12 @@ export default function RealtimeAssistant() {
   const appContextRef = useRef(appContext);
   const { settings } = useSettings();
   const { timers } = useTimers();
+  const [assistantResponding, setAssistantResponding] = useState(false);
   const autoEndConversationRef = useRef(false);
-
-  // TODO: Remove this hack
-  const spotifyContext = useSpotifyContext();
-  useEffect(() => {
-    if (spotifyContext.player && spotifyContext.accessToken && spotifyContext.deviceId) {
-      appContextRef.current.setSpotify({
-        player: spotifyContext.player,
-        accessToken: spotifyContext.accessToken,
-        deviceId: spotifyContext.deviceId,
-        search: spotifyContext.search,
-        play: spotifyContext.play,
-        playTopTracks: spotifyContext.playTopTracks,
-        pausePlayback: spotifyContext.pausePlayback,
-      });
-    } else {
-      appContextRef.current.setSpotify(undefined);
-    }
-  }, [
-    spotifyContext.player,
-    spotifyContext.accessToken,
-    spotifyContext.deviceId,
-    spotifyContext.search,
-    spotifyContext.play,
-    spotifyContext.playTopTracks,
-    spotifyContext.pausePlayback,
-  ]);
-  // End hack
+  const assistantRespondingRef = useRef(false);
 
   useEffect(() => {
     appContextRef.current = appContext;
-    console.log("app context updated");
   }, [appContext]);
 
   /**
@@ -91,6 +76,7 @@ export default function RealtimeAssistant() {
     await wavStreamPlayer.connect();
 
     autoEndConversationRef.current = false;
+    assistantRespondingRef.current = false;
 
     // Connect to realtime API
     await client.connect();
@@ -124,7 +110,7 @@ export default function RealtimeAssistant() {
       const client = clientRef.current;
       switch (event.data.command) {
         case "process":
-          if (client.getTurnDetectionType() === "server_vad") {
+          if (client.getTurnDetectionType() === "server_vad" && !assistantRespondingRef.current) {
             client.appendInputAudio(event.data.inputFrame);
           }
           break;
@@ -166,105 +152,120 @@ export default function RealtimeAssistant() {
 
 Make sure to use the 'end_conversation' tool after you have completed your task and when the user no longer requires you.`;
     client.updateSession({ instructions });
-  }, [appContext.location, appContext.spotify, timers]);
+  }, [appContext.location, timers]);
 
   /**
    * Core RealtimeClient and audio capture setup
    * Set all of our instructions, tools, events and more
    */
-  useEffect(() => {
-    // Get refs
-    const wavStreamPlayer = wavStreamPlayerRef.current;
-    const client = clientRef.current;
+  const setupClient = useCallback(
+    async (client: RealtimeClient, settings: Settings) => {
+      // Get refs
+      const wavStreamPlayer = wavStreamPlayerRef.current;
 
-    // Set transcription, otherwise we don't get user transcriptions back
-    client.updateSession({
-      input_audio_transcription: { model: "whisper-1" },
-    });
-    // Activate server-side turn detection
-    client.updateSession({
-      turn_detection: {
-        type: "server_vad",
-        threshold: 0.5,
-        prefix_padding_ms: 300,
-        silence_duration_ms: 200,
-      },
-    });
-
-    // Add tools
-    const tools = getTools(settings, appContextRef.current);
-    for (const tool of tools) {
-      console.log(`adding tool "${tool.function.name}"`);
-      client.addTool(tool.function as ToolDefinitionType, async (args: never) => {
-        const result = await callFunction(
-          { name: tool.function.name, arguments: JSON.stringify(args) },
-          appContextRef.current,
-        );
-        console.log("function result", result);
-        return result;
+      // Set transcription, otherwise we don't get user transcriptions back
+      client.updateSession({
+        input_audio_transcription: { model: "whisper-1" },
       });
-    }
+      // Activate server-side turn detection
+      client.updateSession({
+        turn_detection: {
+          type: "server_vad",
+          threshold: 0.5,
+          prefix_padding_ms: 300,
+          silence_duration_ms: 200,
+        },
+      });
 
-    client.addTool(
-      {
-        name: "end_conversation",
-        description:
-          "Ends the current conversation with the user. Use when the task is done or the user indicates the assistant is no longer needed.",
-        parameters: {},
-      },
-      async () => {
-        autoEndConversationRef.current = true;
-        return { ok: true };
-      },
-    );
+      // Add tools
+      const tools = await getTools(settings);
+      for (const tool of tools) {
+        console.log(`adding tool "${tool.function.name}"`);
+        client.addTool(tool.function as ToolDefinitionType, async (args: never) => {
+          const result = await callFunction({ name: tool.function.name, arguments: JSON.stringify(args) });
+          console.log("function result", result);
+          return result;
+        });
+      }
 
-    client.on("error", (event: never) => console.error(event));
-    client.on("conversation.interrupted", async () => {
-      const trackSampleOffset = await wavStreamPlayer.interrupt();
-      if (trackSampleOffset?.trackId) {
-        const { trackId, offset } = trackSampleOffset;
-        client.cancelResponse(trackId, offset);
-      }
-    });
-    client.on("conversation.updated", async ({ item, delta }: RealtimeEvent) => {
-      const items = client.conversation.getItems();
-      if (delta?.audio) {
-        wavStreamPlayer.add16BitPCM(delta.audio, item.id);
-      }
-      // if (item.status === "completed" && item.formatted.audio?.length) {
-      //   const wavFile = await WavRecorder.decode(item.formatted.audio, 24000, 24000);
-      //   item.formatted.file = wavFile;
-      // }
-      setItems(items);
-    });
-    client.on("conversation.item.completed", async ({ item }: RealtimeEvent) => {
-      const items = client.conversation.getItems();
-      setItems(items);
-      if (autoEndConversationRef.current && item.role === "assistant") {
-        disconnectConversation();
-      }
-    });
-    client.on("server.input_audio_buffer.speech_started", async () => {
-      console.log("user speech started");
-    });
-    client.on("server.input_audio_buffer.speech_stopped", async () => {
-      console.log("user speech started");
-    });
+      client.addTool(
+        {
+          name: "end_conversation",
+          description:
+            "Ends the current conversation with the user. Use when the task is done or the user indicates the assistant is no longer needed.",
+          parameters: {},
+        },
+        async () => {
+          console.log("calling function:", "end_conversation");
+          autoEndConversationRef.current = true;
+          return { ok: true };
+        },
+      );
 
-    setItems(client.conversation.getItems());
+      client.on("error", (event: never) => console.error(event));
+      client.on("conversation.interrupted", async () => {
+        const trackSampleOffset = await wavStreamPlayer.interrupt();
+        if (trackSampleOffset?.trackId) {
+          const { trackId, offset } = trackSampleOffset;
+          client.cancelResponse(trackId, offset);
+        }
+      });
+      client.on("conversation.updated", async ({ item, delta }: RealtimeEvent) => {
+        const items = client.conversation.getItems();
+        if (delta?.audio) {
+          wavStreamPlayer.add16BitPCM(delta.audio, item.id);
+        }
+        // if (item.status === "completed" && item.formatted.audio?.length) {
+        //   const wavFile = await WavRecorder.decode(item.formatted.audio, 24000, 24000);
+        //   item.formatted.file = wavFile;
+        // }
+        setItems(items);
+      });
+      client.on("conversation.item.completed", async ({ item }: RealtimeEvent) => {
+        const items = client.conversation.getItems();
+        setItems(items);
+        if (autoEndConversationRef.current && item.role === "assistant" && item.type !== "function_call") {
+          autoEndConversationRef.current = false;
+          disconnectConversation();
+        }
+      });
+      client.on("realtime.event", async (event: ClientEvent) => {
+        const { type } = event.event;
+        switch (type) {
+          case "response.created":
+            console.log("response.created", JSON.stringify(event.event, null, 2));
+            setAssistantResponding(true);
+            assistantRespondingRef.current = true;
+            break;
+          case "response.done":
+            console.log("response.done", JSON.stringify(event.event, null, 2));
+            setAssistantResponding(false);
+            assistantRespondingRef.current = false;
+            break;
+        }
+      });
+    },
+    [disconnectConversation],
+  );
+
+  useEffect(() => {
+    const client = clientRef.current;
+    setupClient(client, settings).then(() => {
+      setItems(client.conversation.getItems());
+    });
 
     return () => {
       // cleanup; resets to defaults
       client.reset();
     };
-  }, [disconnectConversation, settings]);
+  }, [settings, setupClient]);
 
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
     const convertedItems: Message[] = [];
     for (const item of items) {
-      if (item.type === "message" && item.status === "completed") {
+      if (item.type === "message" /* && item.status === "completed"*/) {
         convertedItems.push({
           id: item.id,
           role: item.role,
@@ -303,9 +304,14 @@ Make sure to use the 'end_conversation' tool after you have completed your task 
                 <MicIcon />
               </IconButton>
             )}
-            {isConnected && (
+            {isConnected && !assistantResponding && (
               <IconButton area-label="stop conversation" color={"error"} onClick={disconnectConversation}>
                 <RecordVoiceOverIcon />
+              </IconButton>
+            )}
+            {isConnected && assistantResponding && (
+              <IconButton area-label="stop conversation" onClick={disconnectConversation}>
+                <RiRobot2Fill />
               </IconButton>
             )}
           </div>
