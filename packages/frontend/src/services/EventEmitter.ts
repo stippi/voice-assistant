4b67e5235cc9116dcd66c743d5a313a7582630b0
Stephan Aßmus <stephan.assmus@sap.com>
2024-10-26 17:29:26 +0200

    Base class for EventEmitters

---
+++ packages/frontend/src/services/EventEmitter.ts
@@ -0,0 +1,40 @@
+/** Base type for the event map */
+type EventMap = {
+  [key: string]: unknown[];
+};
+
+type EventKey<T extends EventMap> = string & keyof T;
+type EventCallback<T extends EventMap, K extends EventKey<T>> = (...args: T[K]) => void;
+
+export class EventEmitter<T extends EventMap> {
+  private events: {
+    [K in EventKey<T>]?: EventCallback<T, K>[];
+  } = {};
+
+  on<K extends EventKey<T>>(event: K, callback: EventCallback<T, K>): void {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event]?.push(callback);
+  }
+
+  off<K extends EventKey<T>>(event: K, callback: EventCallback<T, K>): void {
+    if (!this.events[event]) return;
+    this.events[event] = this.events[event]?.filter((cb) => cb !== callback) as EventCallback<T, K>[];
+  }
+
+  emit<K extends EventKey<T>>(event: K, ...args: T[K]): void {
+    if (!this.events[event]) return;
+    this.events[event]?.forEach((callback) => {
+      callback(...args);
+    });
+  }
+
+  removeAllListeners(event?: EventKey<T>): void {
+    if (event) {
+      delete this.events[event];
+    } else {
+      this.events = {};
+    }
+  }
+}
